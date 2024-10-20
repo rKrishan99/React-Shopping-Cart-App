7774d4de481965fe253b453de1a8e8894261b20b
RajithaKWG <rkrishan894@gmail.com>
2024-10-20 18:03:42 +0530

    wrapped App using ShoppingCartProvider

--- shopping-cart-app/src/main.jsx
+++ shopping-cart-app/src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import ShoppingCartProvider from "./context/Context.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App/>
-  </BrowserRouter>,
-)
+    <ShoppingCartProvider>
+      <App />
+    </ShoppingCartProvider>
+  </BrowserRouter>
+);

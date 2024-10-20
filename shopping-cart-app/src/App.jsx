f9e3a02b8f9c43c0a73f3ec37635f695a7efa6d5
RajithaKWG <rkrishan894@gmail.com>
2024-10-20 17:56:53 +0530

    setup page routing

--- shopping-cart-app/src/App.jsx
+++ shopping-cart-app/src/App.jsx
@@ -1,10 +1,19 @@
 import { Fragment } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import ProducrList from './pages/productList/ProducrList'
+import ProductDetails from './pages/productDetails/ProductDetails'
+import CartList from './pages/cartList/CartList'
 
 
 function App() {
 
   return (
     <Fragment>
+      <Routes>
+        <Route path='/product-list' element={<ProducrList/>} />
+        <Route path='/product-details/:id' element={<ProductDetails/>} />
+        <Route path='/cart' element={<CartList/>} />
+      </Routes>
 
     </Fragment>
   )

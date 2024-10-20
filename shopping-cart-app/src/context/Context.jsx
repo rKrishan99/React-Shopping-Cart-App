133f0cd89f4ded3e2729fb9c5b5052085a24a125
RajithaKWG <rkrishan894@gmail.com>
2024-10-20 20:36:01 +0530

    created product list

--- shopping-cart-app/src/context/Context.jsx
+++ shopping-cart-app/src/context/Context.jsx
@@ -3,28 +3,36 @@ import React, { createContext, useEffect, useState } from "react";
 export const ShoppingCartContext = createContext(null);
 
 function ShoppingCartProvider({ children }) {
-
-  const [loading, setloading] = useState(false);
+  const [loading, setloading] = useState(true);
   const [listOfProducts, setlistOfProducts] = useState([]);
+  const [productDetails, setproductDetails] = useState(null);
 
-    async function fetchListOfProducts() {
-        const apiResponse = await fetch('https://dummyjson.com/products');
-        const result = await apiResponse.json();
+  async function fetchListOfProducts() {
+    const apiResponse = await fetch("https://dummyjson.com/products");
+    const result = await apiResponse.json();
 
-        if(result && result?.products){
-            setlistOfProducts(result?.products);
-        }
-
+    if (result && result?.products) {
+      setlistOfProducts(result?.products);
+      setloading(false);
     }
+  }
 
-    useEffect(() => {
-        fetchListOfProducts(listOfProducts);
-    },[]);
+  useEffect(() => {
+    fetchListOfProducts(listOfProducts);
+  }, []);
 
-    console.log(listOfProducts);
+  console.log(listOfProducts);
 
   return (
-    <ShoppingCartContext.Provider value={{listOfProducts}}>
+    <ShoppingCartContext.Provider
+      value={{
+        listOfProducts,
+        loading,
+        setloading,
+        productDetails,
+        setproductDetails,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
--- shopping-cart-app/src/pages/productList/ProductList.jsx
+++ shopping-cart-app/src/pages/productList/ProductList.jsx
@@ -1,13 +1,32 @@
 import React, { useContext } from "react";
 import { ShoppingCartContext } from "../../context/Context";
+import ProductTitle from "../../components/productTitle/ProductTitle";
 
 const ProductList = () => {
-
-  const {listOfProducts} = useContext(ShoppingCartContext);
+  const { listOfProducts, loading } = useContext(ShoppingCartContext);
 
   console.log(listOfProducts);
 
-  return <div>ProducrList</div>;
+  if(loading) return <h1>Loading data! Please wait.</h1>
+
+  return (
+    <section className="py-12 bg-white sm:py-16 lg:py-20">
+      <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div className="max-w-md mx-auto text-center">
+          <h2 className="text-3xl font-extralight text-gray-950 sm:text-4xl">
+            Our Featured Products
+          </h2>
+        </div>
+        <div className="grid grid-col-2 gap-5 mt-10 lg:mt-16 lg:gap-8 lg:grid-cols-4">
+          {
+            listOfProducts && listOfProducts.length > 0 ?
+            listOfProducts.map(singleProductTitle => <ProductTitle singleProductTitle={singleProductTitle} />) :
+            <h3>Product Not Found!</h3>
+          }
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProductList;
